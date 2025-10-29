24dbd0f8349c56888237074df660349e48ac8439
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-10-29 22:00:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 export DISPLAY=:0
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
-MESA_NO_ERROR=1 taskset -c 4-7 virgl_test_server_android &
+virgl_test_server_android &
 termux-x11 :0 &
 pd sh alpine --user user --shared-tmp -- bash -c '
     export DISPLAY=:0
     export XDG_RUNTIME_DIR=/tmp
+    export GALLIUM_DRIVER=virpipe
+    export MESA_GL_VERSION_OVERRIDE=4.0
     openbox &
     cage phoc -E "/usr/libexec/phosh"
 '
