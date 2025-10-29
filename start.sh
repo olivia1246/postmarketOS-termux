113bc0d76d3964228de911d4b3f73de087717525
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-10-29 03:03:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 virgl_test_server_android &
 termux-x11 :0 &
 pd sh alpine --user user --shared-tmp -- sh -c '
-export DISPLAY=:0
-export XDG_RUNTIME_DIR=/tmp
-openbox &
-cage phoc -E "/usr/libexec/phosh" &
+    export DISPLAY=:0
+    export XDG_RUNTIME_DIR=/tmp
+    openbox &
+    cage phoc -E "/usr/libexec/phosh" &
 '
