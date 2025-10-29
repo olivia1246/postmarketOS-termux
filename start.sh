fe1af9843c71126a381c27921863cbb17e0a175e
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-10-29 03:07:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 virgl_test_server_android &
 termux-x11 :0 &
-pd sh alpine --shared-tmp -- sh -c '
+pd sh alpine --user user --shared-tmp -- sh -c '
     export DISPLAY=:0
     export XDG_RUNTIME_DIR=/tmp
     openbox &
